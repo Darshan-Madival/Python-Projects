96bf27e9a0918166a2aa4ac50de91e08294896bb
Darshan <darshanmadival.sirsi@gmail.com>
2022-08-17 17:44:23 +0530

    Dice-rolling-project.py added

---
+++ Dice-rolling-project.py
@@ -0,0 +1,15 @@
+import random
+
+
+Again="yes"
+while(Again=="yes"):
+  print("Dice is Rolling Dude Wait.....")
+  print("The values Are: ")
+  a=random.randint(1,6)
+  print(a)
+  b=random.randint(1,6)d
+  print(b)
+  Again = input("Do You Want To Roll The Dice Again?")
+
+
+
